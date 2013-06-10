2dec8bb5e9cdd2ff640d831a111b976b87cfa53c
Simon Kers <skers@kth.se>
2013-06-10 14:10:22 +0200

    Automatically convert tex to eps

--- flowchart/generate.sh
+++ flowchart/generate.sh
@@ -1,5 +1,31 @@
-#!/bin/sh
-pdflatex before.tex
-pdftops -eps before.pdf ../final/before.eps
-pdflatex after.tex
-pdftops -eps after.pdf ../final/after.eps
+#!/bin/bash
+#just add filenames between EOF tags
+
+preamble() {
+    echo "\documentclass{article}" > tmp.tex
+    echo "\usepackage[margin=0in]{geometry}" >> tmp.tex
+    echo "\usepackage{tikz}" >> tmp.tex
+    echo "\begin{document}" >> tmp.tex
+    cat $1.tex >> tmp.tex
+    echo "\end{document}" >> tmp.tex
+}
+
+render() {
+    pdflatex tmp.tex
+    cp tmp.pdf $1.pdf
+    pdftops -eps tmp.pdf ../final/$1.eps
+}
+
+while read -r file; do
+    if grep -q "documentclass" $file.tex; then
+        cat $file.tex > tmp.tex
+    else
+        preamble $file
+    fi
+    render $file
+done <<EOF
+    before
+    after
+    wizard-seq_dia
+EOF
+
